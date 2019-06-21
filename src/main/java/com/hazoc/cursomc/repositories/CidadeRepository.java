e371ff787465f9fb82a7ce09ffcfdd9e249acf9c
Daniel Green <hazoccc@gmail.com>
2019-06-20 22:19:50 -0300

    Estado e cidade

---
+++ src/main/java/com/hazoc/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.hazoc.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hazoc.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
