39d4c52588c37e91017c33a9b4a5789a3fb8591e
Daniel Green <hazoccc@gmail.com>
2019-06-20 21:20:00 -0300

    Produto associacao muitos para muitos

---
+++ src/main/java/com/hazoc/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.hazoc.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hazoc.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
