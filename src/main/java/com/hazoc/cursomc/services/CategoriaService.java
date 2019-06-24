17221718979612834d87871adcdf5e790931b190
Hazoc <hazoccc@gmail.com>
2019-06-24 09:47:53 -0300

    Deletenado categoria com DELETE

--- src/main/java/com/hazoc/cursomc/services/CategoriaService.java
+++ src/main/java/com/hazoc/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.hazoc.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.hazoc.cursomc.domain.Categoria;
 import com.hazoc.cursomc.repositories.CategoriaRepository;
+import com.hazoc.cursomc.services.exceptions.DataIntegrityException;
 import com.hazoc.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não e possive excluir a categoria pois possui produtos associados.");
+		}
 	}
 }
