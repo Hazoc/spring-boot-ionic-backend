aa6c55954137209302d88228b1ad9a423f317a97
Hazoc <hazoccc@gmail.com>
2019-10-02 09:56:08 -0300

    Implementando autorizacao

--- src/main/java/com/hazoc/cursomc/config/SecurityConfig.java
+++ src/main/java/com/hazoc/cursomc/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.hazoc.cursomc.security.JWTAuthenticationFilter;
+import com.hazoc.cursomc.security.JWTAuthorizationFilter;
 import com.hazoc.cursomc.security.JWTUtil;
 
 
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
