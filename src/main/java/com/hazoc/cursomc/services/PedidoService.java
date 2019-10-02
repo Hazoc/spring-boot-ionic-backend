fce61da957d9d7f331bf926d9a2ab173d18f7379
Hazoc <hazoccc@gmail.com>
2019-10-02 11:04:25 -0300

    Restricao de conteudo: cliente recupera seus pedidos

--- src/main/java/com/hazoc/cursomc/services/PedidoService.java
+++ src/main/java/com/hazoc/cursomc/services/PedidoService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +20,8 @@ import com.hazoc.cursomc.domain.enums.EstadoPagamento;
 import com.hazoc.cursomc.repositories.ItemPedidoRepository;
 import com.hazoc.cursomc.repositories.PagamentoRepository;
 import com.hazoc.cursomc.repositories.PedidoRepository;
+import com.hazoc.cursomc.security.UserSS;
+import com.hazoc.cursomc.services.exceptions.AuthorizationException;
 import com.hazoc.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -71,5 +76,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+		
 	}
 }
