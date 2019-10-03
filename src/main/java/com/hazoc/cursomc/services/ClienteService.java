61c8fc73204ab4712a10d11d403f09090c6d57cb
Hazoc <hazoccc@gmail.com>
2019-10-03 08:35:41 -0300

    Enviando imagem via endpoint

--- src/main/java/com/hazoc/cursomc/services/ClienteService.java
+++ src/main/java/com/hazoc/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.hazoc.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.hazoc.cursomc.domain.Cidade;
 import com.hazoc.cursomc.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -106,5 +111,9 @@ public class ClienteService {
 			cli.getTelefones().add(objDTO.getTelefone3());	
 		}
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);		
 	}
 }
